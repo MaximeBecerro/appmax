d4db1f4f37ad7bd451744175c851f9852a20caa3
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2019-03-15 17:47:08 +0100

    fixed music

--- src/music.js
+++ src/music.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 class Music extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            songs: [
-                {
-                    url: "/song/11 De Visée - Suite in C minor (La Plainte, ou Tombeau de Mesdemoiselles de Visée, filles de l'auteur).wma"
-                }
-            ],
-        };
-    }
     render() {
         return (
             <div className="music">
                 <ReactAudioPlayer
-                src="devisee.mp3"
+                src="/song/devisee.mp3"
                 autoPlay
+                loop
                 />
             </div>
         );
