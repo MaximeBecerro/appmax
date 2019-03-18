a615135768f7490a894219dde19faabf2ccda01b
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2019-03-18 09:06:03 +0100

    removed app link

--- src/App.js
+++ src/App.js
@@ -18,14 +18,6 @@ class App extends Component {
           <p>
             <code>Hello world!</code>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <Music />
         </main>
         <footer className="App-footer">
